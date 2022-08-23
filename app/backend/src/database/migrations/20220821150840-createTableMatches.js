27c7bcf407e335651f425da118f77a540b0f1ee4
cathe <cacateles12@gmail.com>
2022-08-23 14:50:38 -0300

    Fixed migration type from NUMBER to INTEGER

--- app/backend/src/database/migrations/20220821150840-createTableMatches.js
+++ app/backend/src/database/migrations/20220821150840-createTableMatches.js
@@ -11,26 +11,30 @@ module.exports = {
       homeTeam: {
         type: Sequelize.STRING,
         allowNull: false,
+        field: 'home_team',
         references: {
           model: 'teams',
           key: 'id',
         },
       },
       homeTeamGoals: {
-        type: Sequelize.NUMBER,
-        allowNull: false
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
       },
       awayTeam: {
         type: Sequelize.STRING,
         allowNull: false,
+        field: 'away_team',
         references: {
           model: 'teams',
           key: 'id',
         },
       },
       awayTeamGoals: {
-        type: Sequelize.NUMBER,
-        allowNull: false
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals',
       },
       inProgress: {
         type: Sequelize.BOOLEAN,
