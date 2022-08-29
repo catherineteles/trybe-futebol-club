53f2b43a267a0742de8c1c4275a3bd0a3f9c2fa1
cathe <cacateles12@gmail.com>
2022-08-29 16:07:31 -0300

    Added association Team hasMany Match

--- app/backend/src/database/models/MatchesModel.ts
+++ app/backend/src/database/models/MatchesModel.ts
@@ -44,7 +44,8 @@ Match.init({
   underscored: true,
 });
 
-Team.hasMany(Match, { foreignKey: 'id', as: 'matchesId' });
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeMatches' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayMatches' });
 
 Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
 Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
