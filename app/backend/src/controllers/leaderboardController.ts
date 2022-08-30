cb2f64ddc32e10a89338ff1de976cae81d4584c9
cathe <cacateles12@gmail.com>
2022-08-30 11:06:42 -0300

    Sorted teams before sending array

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -4,7 +4,8 @@ import BoardService from '../services/leaderboardService';
 export default class BoardController {
   static listHomeTeams = async (req: Request, res: Response): Promise<void> => {
     const teams = await BoardService.listHome();
+    const orderTeams = teams.sort((a, b) => b.totalPoints - a.totalPoints);
 
-    res.status(200).json(teams);
+    res.status(200).json(orderTeams);
   };
 }
